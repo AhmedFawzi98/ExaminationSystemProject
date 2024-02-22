c4a8212afddc87484c42dd874369003691329c83
Ahmed Fawzi <ahmedfawzielarabi98@gmail.com>
2024-02-22 18:14:46 +0200

    progress in Instructor Form

--- ExaminationSystem/UI/InstructorForm.Designer.cs
+++ ExaminationSystem/UI/InstructorForm.Designer.cs
@@ -28,12 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "InstructorForm";
+            metroSetTabControl1 = new MetroSet_UI.Controls.MetroSetTabControl();
+            tabPage0 = new TabPage();
+            tabPage2 = new TabPage();
+            tabPage3 = new TabPage();
+            gradesGridView = new DataGridView();
+            tabPage4 = new TabPage();
+            tabPage5 = new TabPage();
+            metroSetLabel1 = new MetroSet_UI.Controls.MetroSetLabel();
+            metroSetControlBox1 = new MetroSet_UI.Controls.MetroSetControlBox();
+            metroSetTabControl1.SuspendLayout();
+            tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)gradesGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // metroSetTabControl1
+            // 
+            metroSetTabControl1.AnimateEasingType = MetroSet_UI.Enums.EasingType.CubeOut;
+            metroSetTabControl1.AnimateTime = 200;
+            metroSetTabControl1.BackgroundColor = Color.White;
+            metroSetTabControl1.Controls.Add(tabPage0);
+            metroSetTabControl1.Controls.Add(tabPage2);
+            metroSetTabControl1.Controls.Add(tabPage3);
+            metroSetTabControl1.Controls.Add(tabPage4);
+            metroSetTabControl1.Controls.Add(tabPage5);
+            metroSetTabControl1.IsDerivedStyle = true;
+            metroSetTabControl1.ItemSize = new Size(100, 38);
+            metroSetTabControl1.Location = new Point(15, 73);
+            metroSetTabControl1.Name = "metroSetTabControl1";
+            metroSetTabControl1.SelectedIndex = 2;
+            metroSetTabControl1.SelectedTextColor = Color.White;
+            metroSetTabControl1.Size = new Size(1238, 620);
+            metroSetTabControl1.SizeMode = TabSizeMode.Fixed;
+            metroSetTabControl1.Speed = 100;
+            metroSetTabControl1.Style = MetroSet_UI.Enums.Style.Light;
+            metroSetTabControl1.StyleManager = null;
+            metroSetTabControl1.TabIndex = 0;
+            metroSetTabControl1.ThemeAuthor = "Narwin";
+            metroSetTabControl1.ThemeName = "MetroLite";
+            metroSetTabControl1.UnselectedTextColor = Color.Gray;
+            metroSetTabControl1.UseAnimation = false;
+            // 
+            // tabPage0
+            // 
+            tabPage0.Location = new Point(4, 42);
+            tabPage0.Name = "tabPage0";
+            tabPage0.Size = new Size(1230, 574);
+            tabPage0.TabIndex = 0;
+            tabPage0.Text = "Profile";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 42);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Size = new Size(1230, 574);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Generate Exam";
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(gradesGridView);
+            tabPage3.Location = new Point(4, 42);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Size = new Size(1230, 574);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "Students Grades";
+            // 
+            // gradesGridView
+            // 
+            gradesGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gradesGridView.Location = new Point(3, 3);
+            gradesGridView.Name = "gradesGridView";
+            gradesGridView.RowHeadersWidth = 51;
+            gradesGridView.Size = new Size(1224, 568);
+            gradesGridView.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Location = new Point(4, 42);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Size = new Size(1230, 574);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "Department Details";
+            // 
+            // tabPage5
+            // 
+            tabPage5.Location = new Point(4, 42);
+            tabPage5.Name = "tabPage5";
+            tabPage5.Size = new Size(1230, 574);
+            tabPage5.TabIndex = 4;
+            tabPage5.Text = "Questions Bank";
+            // 
+            // metroSetLabel1
+            // 
+            metroSetLabel1.Font = new Font("Microsoft YaHei UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            metroSetLabel1.IsDerivedStyle = true;
+            metroSetLabel1.Location = new Point(15, 22);
+            metroSetLabel1.Name = "metroSetLabel1";
+            metroSetLabel1.Size = new Size(317, 29);
+            metroSetLabel1.Style = MetroSet_UI.Enums.Style.Light;
+            metroSetLabel1.StyleManager = null;
+            metroSetLabel1.TabIndex = 1;
+            metroSetLabel1.Text = "Instructors Control Panel";
+            metroSetLabel1.ThemeAuthor = "Narwin";
+            metroSetLabel1.ThemeName = "MetroLite";
+            // 
+            // metroSetControlBox1
+            // 
+            metroSetControlBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            metroSetControlBox1.CloseHoverBackColor = Color.FromArgb(183, 40, 40);
+            metroSetControlBox1.CloseHoverForeColor = Color.White;
+            metroSetControlBox1.CloseNormalForeColor = Color.Gray;
+            metroSetControlBox1.DisabledForeColor = Color.DimGray;
+            metroSetControlBox1.IsDerivedStyle = true;
+            metroSetControlBox1.Location = new Point(1153, 6);
+            metroSetControlBox1.MaximizeBox = true;
+            metroSetControlBox1.MaximizeHoverBackColor = Color.FromArgb(238, 238, 238);
+            metroSetControlBox1.MaximizeHoverForeColor = Color.Gray;
+            metroSetControlBox1.MaximizeNormalForeColor = Color.Gray;
+            metroSetControlBox1.MinimizeBox = true;
+            metroSetControlBox1.MinimizeHoverBackColor = Color.FromArgb(238, 238, 238);
+            metroSetControlBox1.MinimizeHoverForeColor = Color.Gray;
+            metroSetControlBox1.MinimizeNormalForeColor = Color.Gray;
+            metroSetControlBox1.Name = "metroSetControlBox1";
+            metroSetControlBox1.Size = new Size(100, 25);
+            metroSetControlBox1.Style = MetroSet_UI.Enums.Style.Light;
+            metroSetControlBox1.StyleManager = null;
+            metroSetControlBox1.TabIndex = 2;
+            metroSetControlBox1.Text = "metroSetControlBox1";
+            metroSetControlBox1.ThemeAuthor = "Narwin";
+            metroSetControlBox1.ThemeName = "MetroLite";
+            // 
+            // InstructorForm
+            // 
+            AutoScaleDimensions = new SizeF(13F, 26F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1268, 708);
+            Controls.Add(metroSetControlBox1);
+            Controls.Add(metroSetLabel1);
+            Controls.Add(metroSetTabControl1);
+            Name = "InstructorForm";
+            SmallLineColor1 = Color.FromArgb(0, 0, 0, 1);
+            SmallLineColor2 = Color.FromArgb(0, 0, 0, 1);
+            Load += InstructorForm_Load;
+            metroSetTabControl1.ResumeLayout(false);
+            tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)gradesGridView).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private MetroSet_UI.Controls.MetroSetTabControl metroSetTabControl1;
+        private MetroSet_UI.Controls.MetroSetLabel metroSetLabel1;
+        private TabPage tabPage0;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private TabPage tabPage5;
+        private DataGridView gradesGridView;
+        private MetroSet_UI.Controls.MetroSetControlBox metroSetControlBox1;
     }
 }
