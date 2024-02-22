264ad6b9acf1ef652a3b304fdc093de3dfac9b32
MostafaAymanAwad <ayelshekh@gmail.com>
2024-02-22 19:15:44 +0200

    Adding CurrentInstructor to Instructor Form

--- ExaminationSystem/UI/LoginForm.cs
+++ ExaminationSystem/UI/LoginForm.cs
@@ -40,7 +40,9 @@ public partial class LoginForm : MetroSetForm
         else
         {
             this.Hide();
-            InstructorForm instructorForm = new InstructorForm(_logger, _examination_SystemContext, _spContext);
+            Instructor currentInstructor = _examination_SystemContext.Instructors.FirstOrDefault(
+                I =>I.Username== username);
+            InstructorForm instructorForm = new InstructorForm(_logger, _examination_SystemContext, _spContext, currentInstructor);
             instructorForm.FormClosed += (sender, e) => this.Close();
             instructorForm.Show();
         }
